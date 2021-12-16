27a866b529c10ab7a68e25b6b5bcc466bad5b6aa
nailen1 <nietsnie1@gmail.com>
2021-12-16 15:05:26 +0900

    마지막행의 식별자 알아내기

--- create.py
+++ create.py
@@ -1,17 +1,14 @@
 import sqlite3
 from sqlite3.dbapi2 import connect
-connect = sqlite3.connect('data.db')
-cursor = connect.cursor()
+with sqlite3.connect('data.db') as connect:
+    cursor = connect.cursor()
 
-title = input('제목을 알려주세요: ')
-body = input('본문을 알려주세요: ')
+    title = input('제목을 알려주세요: ')
+    body = input('본문을 알려주세요: ')
 
-print('title', 'body')
+    print('title', 'body')
 
-sql = 'INSERT INTO topics (title, body) VALUES(?, ?)'
+    sql = 'INSERT INTO topics (title, body) VALUES(?, ?)'
+    cursor.execute(sql, (title, body))
 
-cursor.execute(sql, (title, body))
-connect.commit()
-
-cursor.close()
-connect.close()
+    print('last input id:', cursor.lastrowid)
